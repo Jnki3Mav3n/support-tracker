283266be8fc31c174f35d2981ba3e9bee495efe1
Jnki3Mav3n <manu.ar217@gmail.com>
2021-05-19 19:38:35 -0600

    Added basic directories and files for Redux implementation

---
+++ frontend/src/controllers/redux/authSlice.js
@@ -0,0 +1,20 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+const slice = createSlice({
+    name:"auth",
+    initialState:{
+        admin:false,
+        LoggedIn:false,
+    },
+    reducers:{
+        signIn:(state, action)=>{
+
+        },
+        signOut:(state)=>{
+
+        },
+        createUser:(state,action)=>{
+            
+        }
+    }
+})
