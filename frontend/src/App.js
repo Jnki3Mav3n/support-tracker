e67f224d1a37e2f06c48669fa5c74bd6fc0b5dc8
Jnki3Mav3n <manu.ar217@gmail.com>
2021-05-23 03:03:41 -0600

    Login homepage and functions were added with assigned CSS styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 // import React from 'react';
-import './App.css';
+import Login from './views/login/login'
+import {useSelector} from 'react-redux'
 
 function App() {
+  const {auth} = useSelector(state => state)
   return (
     <>
+    {!auth.LoggedIn ? <Login /> :
+    <h1>Hello</h1>
+    }
     </>
   );
 }
